0bb19d7a83d84156272f90c93ec1f39bc55b5ac2
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-29 14:49:01 -0700

    Update main.rs
    Fix panic_handler placement
    
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::panic::PanicInfo;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     const VGA: *mut u16 = 0xB8000 as *mut u16;
@@ -13,6 +15,12 @@ pub extern "C" fn _start() -> ! {
     }
     loop { unsafe { core::arch::asm!("hlt") } }
 }
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 
 
 
