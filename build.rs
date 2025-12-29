b5dd4a3afbc9417fc67ced543280eba3c59eb837
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-29 16:21:27 -0700

    Update build.rs
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 fn main() {
     println!("cargo:rustc-link-search=native=src/arch/x86_64");
     println!("cargo:rerun-if-changed=src/arch/x86_64/boot.S");
-    cc::Build::new()
-        .file("src/arch/x86_64/boot.S")
-        .compile("boot");
+        cc::Build::new()
+        .file("src/arch/x86_64/bootsector.S")
+        .flag("-m32")
+        .compile("bootsector");
+
+        
 }
