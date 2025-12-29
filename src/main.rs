bebb4b0c89fea861753005915eada404383542ec
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-29 14:21:51 -0700

    Update main.rs
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-#![no_std]
-#![no_main]
-
-use core::panic::PanicInfo;
-
-const VGA_ADDR: *mut u16 = 0xB8000 as *mut u16;
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    const VGA: *mut u16 = 0xB8000 as *mut u16;
+    const COLOR: u16 = 0x1F; // blue bg, white fg
+    let msg = b"lex-kernel v0.1.0 booted!";
     unsafe {
-        // blue background, white foreground, char 'L'
-        *VGA_ADDR = 0x1F4C;
+        for (i, &ch) in msg.iter().enumerate() {
+            *VGA.add(i) = COLOR << 8 | u16::from(ch);
+        }
     }
-    loop {
-        unsafe { core::arch::asm!("hlt") }
-    }
+    loop { unsafe { core::arch::asm!("hlt") } }
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
