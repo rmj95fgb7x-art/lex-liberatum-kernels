3114439639a1ace64c671638deb1ab6050ab3336
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-29 07:59:50 -0700

    Update main.rs
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-#![no_std]      // don’t link Rust std
-#![no_main]     // disable Rust-level main
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
+const VGA_ADDR: *mut u16 = 0xB8000 as *mut u16;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // TODO: print something
-    loop {}
+    unsafe {
+        // blue background, white foreground, char 'L'
+        *VGA_ADDR = 0x1F4C;
+    }
+    loop {
+        unsafe { core::arch::asm!("hlt") }
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
