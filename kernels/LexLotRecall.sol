751a126ac7435522d16cea9ccf5a10a03f1414f8
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-31 15:49:16 -0700

    Create LexLotRecall.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexLotRecall.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.25;
+import "../src/RoyaltySplitter.sol";
+contract LexLotRecall is RoyaltySplitter {
+    bool    public constant RECALL_ACTIVE = false; // must NOT be under recall
+    uint256 public constant MAX_AGE_DAYS  = 30;    // ≤ 30 days to expiry
+    uint256 public constant GAS_PER_CALL  = 85_000;
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+    function checkLotRecall(bool recalled, uint256 ageDays) external payable {
+        uint256 royaltyWei = (85_000 * block.basefee * 85 * 25) / 1_000_000;
+        bool ok = (!recalled) && (ageDays <= MAX_AGE_DAYS);
+        if (!ok) _splitRoyalty{value: royaltyWei}();
+    }
+    function vertical() external pure returns (string memory) { return "LexLotRecall-Healthcare"; }
+}
