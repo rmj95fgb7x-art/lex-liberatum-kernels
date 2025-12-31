7ad3d98b88efb3bc767cf1a54c56203de5b95869
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-31 15:39:51 -0700

    Create LexReEntry.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexReEntry.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.25;
+import "../src/RoyaltySplitter.sol";
+contract LexReEntry is RoyaltySplitter {
+    uint256 public constant MAX_APOGEE_KM = 400; // ≤ 400 km apogee
+    uint256 public constant GAS_PER_CALL  = 95_000;
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+    function checkReEntry(uint256 apogeeKm) external payable {
+        uint256 royaltyWei = (95_000 * block.basefee * 95 * 25) / 1_000_000;
+        if (apogeeKm > MAX_APOGEE_KM) _splitRoyalty{value: royaltyWei}();
+    }
+    function vertical() external pure returns (string memory) { return "LexReEntry-Space"; }
+}
