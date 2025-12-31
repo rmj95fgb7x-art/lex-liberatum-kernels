9d1f8e0f7c3a9f4a1712862012c81703b8712de0
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-31 15:59:12 -0700

    Create LexReturn.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexReturn.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.25;
+import "../src/RoyaltySplitter.sol";
+contract LexReturn is RoyaltySplitter {
+    uint256 public constant MAX_RETURN_DAYS = 30;
+    uint256 public constant GAS_PER_CALL    = 65_000;
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+    function checkReturn(uint256 daysSincePurchase) external payable {
+        uint256 royaltyWei = (65_000 * block.basefee * 65 * 25) / 1_000_000;
+        if (daysSincePurchase > MAX_RETURN_DAYS) _splitRoyalty{value: royaltyWei}();
+    }
+    function vertical() external pure returns (string memory) { return "LexReturn-Retail"; }
+}
