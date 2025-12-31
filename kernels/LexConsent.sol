5dfad3bcbf4422198f6252e7f7eca8e4ed44f788
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-31 15:49:56 -0700

    Create LexConsent.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexConsent.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.25;
+import "../src/RoyaltySplitter.sol";
+contract LexConsent is RoyaltySplitter {
+    uint256 public constant MIN_SIG_VERSION = 2024;
+    uint256 public constant GAS_PER_CALL    = 75_000;
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+    function checkConsent(uint256 sigVersion) external payable {
+        uint256 royaltyWei = (75_000 * block.basefee * 75 * 25) / 1_000_000;
+        if (sigVersion < MIN_SIG_VERSION) _splitRoyalty{value: royaltyWei}();
+    }
+    function vertical() external pure returns (string memory) { return "LexConsent-Healthcare"; }
+}
