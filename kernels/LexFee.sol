119d09306da8d8024e655d12546a0098e67af792
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-31 15:31:07 -0700

    Create LexFee.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexFee.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.25;
+import "../src/RoyaltySplitter.sol";
+contract LexFee is RoyaltySplitter {
+    uint256 public constant MAX_FEE_USD = 1000;
+    bool    public constant WAIVER_OK   = true;
+    uint256 public constant GAS_PER_CALL= 65_000;
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+    function checkFee(uint256 feeUSD, bool waiverApproved) external payable {
+        uint256 royaltyWei = (65_000 * block.basefee * 65 * 25) / 1_000_000;
+        bool ok = (feeUSD <= MAX_FEE_USD) || (waiverApproved == WAIVER_OK);
+        if (!ok) _splitRoyalty{value: royaltyWei}();
+    }
+    function vertical() external pure returns (string memory) { return "LexFee-Courts"; }
+}
