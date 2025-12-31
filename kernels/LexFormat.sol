4184ced10ae5d8b9d9319fa26f47e7da810a2afe
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-31 15:32:02 -0700

    Create LexFormat.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexFormat.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.25;
+import "../src/RoyaltySplitter.sol";
+contract LexFormat is RoyaltySplitter {
+    bytes3  public constant PDF_SUBTYPE = "A-3";
+    uint256 public constant MIN_FONT_PT = 14;
+    uint256 public constant GAS_PER_CALL= 70_000;
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+    function checkFormat(bytes3 pdfSubtype, uint256 fontPt) external payable {
+        uint256 royaltyWei = (70_000 * block.basefee * 70 * 25) / 1_000_000;
+        bool ok = (pdfSubtype == PDF_SUBTYPE) && (fontPt >= MIN_FONT_PT);
+        if (!ok) _splitRoyalty{value: royaltyWei}();
+    }
+    function vertical() external pure returns (string memory) { return "LexFormat-Courts"; }
+}
