bf28ac6ace0c9fbf36ec6fc054ade7f8d95ab2eb
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2025-12-30 20:21:09 -0700

    Create LexPay.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

---
+++ kernels/LexPay.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Patent-Pending
+pragma solidity ^0.8.25;
+
+import "../src/RoyaltySplitter.sol";
+
+/// @title LexPay
+/// @notice 25 bp royalty on payment-compliance checks:
+///         PCI DSS nonce sequencing, AML velocity, sanctions screening.
+contract LexPay is RoyaltySplitter {
+    // Example thresholds
+    uint256 public constant MAX_NONCE_GAP        = 10;    // missing nonces
+    uint256 public constant MAX_DAILY_VOLUME_USD = 10_000; // $10 k velocity
+    uint256 public constant GAS_PER_CALL         = 90_000;
+
+    constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
+
+    /// @param missingNonces   Count of missing PCI nonces in sequence
+    /// @param dailyVolumeUSD  Daily tx volume (USD)
+    /// @param sanctioned      True if counter-party is on OFAC list
+    function checkPay(uint256 missingNonces, uint256 dailyVolumeUSD, bool sanctioned) external payable {
+        uint256 gasUsed = GAS_PER_CALL;
+        uint256 baseFee = block.basefee;
+        uint256 royaltyWei = gasUsed * baseFee * 90 * 25 / 1_000_000; // 0.90 multiplier
+
+        bool compliant = (missingNonces <= MAX_NONCE_GAP) &&
+                         (dailyVolumeUSD <= MAX_DAILY_VOLUME_USD) &&
+                         (!sanctioned);
+
+        if (!compliant) {
+            _splitRoyalty{value: royaltyWei}();
+        }
+    }
+
+    function vertical() external pure returns (string memory) {
+        return "LexPay-PCI-AML";
+    }
+}
