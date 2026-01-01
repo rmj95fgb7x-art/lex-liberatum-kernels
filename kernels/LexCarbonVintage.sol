25657bc488809dbdad539858b5f01d05b39617ff
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 16:19:58 -0700

    Update LexCarbonVintage.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexCarbonVintage.sol
+++ kernels/LexCarbonVintage.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.25;
-import "../src/AdaptiveKernelBase.sol";
+import "../src/FlagshipAdaptiveBase.sol";
 import "../src/RoyaltySplitter.sol";
 
-contract LexCarbonVintage is RoyaltySplitter, AdaptiveKernelBase {
+contract LexCarbonVintage is RoyaltySplitter, FlagshipAdaptiveBase {
     uint256 public constant MAX_VINTAGE_AGE_YEARS = 5; // ≤ 5 years old
     uint256 public constant GAS_PER_CALL          = 75_000;
 
