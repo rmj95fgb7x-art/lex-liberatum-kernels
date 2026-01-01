a0bc6d24b1e330afe241f8159be4b6ca5bec8327
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 16:18:53 -0700

    Update LexReinsure.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexReinsure.sol
+++ kernels/LexReinsure.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.25;
-import "../src/AdaptiveKernelBase.sol";
+import "../src/FlagshipAdaptiveBase.sol";
 import "../src/RoyaltySplitter.sol";
 
-contract LexReinsure is RoyaltySplitter, AdaptiveKernelBase {
+contract LexReinsure is RoyaltySplitter, FlagshipAdaptiveBase {
     uint256 public constant MAX_COVER_PERCENT = 100; // ≤ 100 % of sum insured
     uint256 public constant GAS_PER_CALL      = 75_000;
 
