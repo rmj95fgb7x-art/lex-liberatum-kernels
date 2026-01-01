741c1fe88999d7c664522e0f315843b7e5ef4c81
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 16:23:36 -0700

    Update LexESGScore.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexESGScore.sol
+++ kernels/LexESGScore.sol
@@ -1,35 +1,33 @@
 pragma solidity ^0.8.25;
-import "../src/AdaptiveKernelBase.sol";
+import "../src/FlagshipAdaptiveBase.sol";
 import "../src/RoyaltySplitter.sol";
 
-contract LexESGScore is RoyaltySplitter, AdaptiveKernelBase {
-    uint256 public constant MIN_SCORE_PERMILLE = 700; // ≥ 70 % ESG score
-    uint256 public constant GAS_PER_CALL       = 80_000;
+contract LexESG is RoyaltySplitter, FlagshipAdaptiveBase {
+    uint256 public constant GAS_PER_CALL = 105_000;
 
     constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
 
-    /// @param scorePermille  ESG score (0–1000)
-    function checkESGScore(uint256 scorePermille) external payable returns (uint256 fused) {
+    /// @param ageYears  Years since carbon-credit vintage
+    function checkESG(uint256 ageYears) external payable returns (uint256 fused) {
         uint256 gasUsed = GAS_PER_CALL;
         uint256 baseFee = block.basefee;
-        uint256 royaltyWei = gasUsed * baseFee * 80 * 25 / 1_000_000; // 0.80 multiplier
+        uint256 royaltyWei = gasUsed * baseFee * 130 * 25 / 1_000_000; // 1.30 multiplier
 
         uint256[] memory signals = new uint256[](1);
-        signals[0] = scorePermille;
+        signals[0] = ageYears;
 
         uint256[] memory distances = new uint256[](1);
-        distances[0] = scorePermille;
+        distances[0] = ageYears;
 
         uint256[] memory weights = adaptiveWeights(distances);
         fused = signals[0] * weights[0] / 10000;
 
-        if (scorePermille < MIN_SCORE_PERMILLE) {
+        if (ageYears > 5) { // 5-year vintage limit
             _splitRoyalty{value: royaltyWei}();
         }
     }
 
     function vertical() external pure returns (string memory) {
-        return "LexESGScore-Adaptive";
+        return "LexESG-Adaptive";
     }
 }
-
