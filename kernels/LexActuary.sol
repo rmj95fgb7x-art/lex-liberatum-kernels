804611ae8466415fbaa87d171be85783e69a4b71
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 15:34:24 -0700

    Update LexActuary.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexActuary.sol
+++ kernels/LexActuary.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.8.25;
+import "../src/AdaptiveKernelBase.sol";
 import "../src/RoyaltySplitter.sol";
-contract LexActuary is RoyaltySplitter {
+
+contract LexActuary is RoyaltySplitter, AdaptiveKernelBase {
     uint256 public constant MAX_TABLE_AGE_YEARS = 5; // ≤ 5 years old
     uint256 public constant GAS_PER_CALL        = 70_000;
+
     constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
-    function checkActuary(uint256 ageYears) external payable {
-        uint256 royaltyWei = (70_000 * block.basefee * 70 * 25) / 1_000_000;
-        if (ageYears > MAX_TABLE_AGE_YEARS) _splitRoyalty{value: royaltyWei}();
+
+    /// @param ageYears  Years since actuarial table publication
+    function checkActuary(uint256 ageYears) external payable returns (uint256 fused) {
+        uint256 gasUsed = GAS_PER_CALL;
+        uint256 baseFee = block.basefee;
+        uint256 royaltyWei = gasUsed * baseFee * 70 * 25 / 1_000_000; // 0.70 multiplier
+
+        uint256[] memory signals = new uint256[](1);
+        signals[0] = ageYears;
+
+        uint256[] memory distances = new uint256[](1);
+        distances[0] = ageYears;
+
+        uint256[] memory weights = adaptiveWeights(distances);
+        fused = signals[0] * weights[0] / 10000;
+
+        if (ageYears > MAX_TABLE_AGE_YEARS) {
+            _splitRoyalty{value: royaltyWei}();
+        }
     }
-    function vertical() external pure returns (string memory) { return "LexActuary-Insurance"; }
+
+    function vertical() external pure returns (string memory) {
+        return "LexActuary-Adaptive";
+    }
 }
