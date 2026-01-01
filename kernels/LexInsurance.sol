0fd8eaef48b59813d3ab185cf36c3df6a3cfd6d3
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 15:32:31 -0700

    Update LexInsurance.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexInsurance.sol
+++ kernels/LexInsurance.sol
@@ -1,13 +1,35 @@
 // SPDX-License-Identifier: Patent-Pending
 pragma solidity ^0.8.25;
+import "../src/AdaptiveKernelBase.sol";
 import "../src/RoyaltySplitter.sol";
-contract LexInsurance is RoyaltySplitter {
+
+contract LexInsurance is RoyaltySplitter, AdaptiveKernelBase {
     uint256 public constant MAX_PREMIUM_DAYS_LATE = 30; // ≤ 30 days overdue
     uint256 public constant GAS_PER_CALL          = 75_000;
+
     constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
-    function checkInsurance(uint256 daysLate) external payable {
-        uint256 royaltyWei = (75_000 * block.basefee * 75 * 25) / 1_000_000;
-        if (daysLate > MAX_PREMIUM_DAYS_LATE) _splitRoyalty{value: royaltyWei}();
+
+    /// @param daysLate  Days since premium due date
+    function checkInsurance(uint256 daysLate) external payable returns (uint256 fused) {
+        uint256 gasUsed = GAS_PER_CALL;
+        uint256 baseFee = block.basefee;
+        uint256 royaltyWei = gasUsed * baseFee * 75 * 25 / 1_000_000; // 0.75 multiplier
+
+        uint256[] memory signals = new uint256[](1);
+        signals[0] = daysLate;
+
+        uint256[] memory distances = new uint256[](1);
+        distances[0] = daysLate;
+
+        uint256[] memory weights = adaptiveWeights(distances);
+        fused = signals[0] * weights[0] / 10000;
+
+        if (daysLate > MAX_PREMIUM_DAYS_LATE) {
+            _splitRoyalty{value: royaltyWei}();
+        }
     }
-    function vertical() external pure returns (string memory) { return "LexInsurance-Insurance"; }
+
+    function vertical() external pure returns (string memory) {
+        return "LexInsurance-Adaptive";
+    }
 }
