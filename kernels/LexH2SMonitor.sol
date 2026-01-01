096d3ca3feb98cb7f5ccee5de14d7cb44977121f
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 15:28:45 -0700

    Update LexH2SMonitor.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexH2SMonitor.sol
+++ kernels/LexH2SMonitor.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.8.25;
+import "../src/AdaptiveKernelBase.sol";
 import "../src/RoyaltySplitter.sol";
-contract LexH2SMonitor is RoyaltySplitter {
-    uint256 public constant MAX_H2S_PPM = 10;
+
+contract LexH2SMonitor is RoyaltySplitter, AdaptiveKernelBase {
+    uint256 public constant MAX_H2S_PPM = 10; // ≤ 10 ppm TWA
     uint256 public constant GAS_PER_CALL= 95_000;
+
     constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
-    function checkH2SMonitor(uint256 h2sPpm) external payable {
-        uint256 royaltyWei = (95_000 * block.basefee * 95 * 25) / 1_000_000;
-        if (h2sPpm > MAX_H2S_PPM) _splitRoyalty{value: royaltyWei}();
+
+    /// @param h2sPpm  Current H₂S reading (ppm)
+    function checkH2SMonitor(uint256 h2sPpm) external payable returns (uint256 fused) {
+        uint256 gasUsed = GAS_PER_CALL;
+        uint256 baseFee = block.basefee;
+        uint256 royaltyWei = gasUsed * baseFee * 95 * 25 / 1_000_000; // 0.95 multiplier
+
+        uint256[] memory signals = new uint256[](1);
+        signals[0] = h2sPpm;
+
+        uint256[] memory distances = new uint256[](1);
+        distances[0] = h2sPpm;
+
+        uint256[] memory weights = adaptiveWeights(distances);
+        fused = signals[0] * weights[0] / 10000;
+
+        if (h2sPpm > MAX_H2S_PPM) {
+            _splitRoyalty{value: royaltyWei}();
+        }
     }
-    function vertical() external pure returns (string memory) { return "LexH2SMonitor-Energy"; }
+
+    function vertical() external pure returns (string memory) {
+        return "LexH2SMonitor-Adaptive";
+    }
 }
