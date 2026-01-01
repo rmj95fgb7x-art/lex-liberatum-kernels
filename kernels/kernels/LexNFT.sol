6f5b5023e01aa806510d50e2c9fc32842d106714
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 16:21:43 -0700

    Update LexNFT.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/kernels/LexNFT.sol
+++ kernels/kernels/LexNFT.sol
@@ -1,20 +1,25 @@
 pragma solidity ^0.8.25;
-import "../src/AdaptiveKernelBase.sol";
+import "../src/FlagshipAdaptiveBase.sol";
 import "../src/RoyaltySplitter.sol";
 
-contract LexNFT is RoyaltySplitter, AdaptiveKernelBase {
+contract LexNFT is RoyaltySplitter, FlagshipAdaptiveBase {
     uint256 public constant GAS_PER_CALL = 95_000;
 
     constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
 
+    /// @param hashDiffs   Array of Hamming distances (scaled ×100)
     function checkNFT(uint256[] memory hashDiffs) external payable returns (uint256 fused) {
         uint256 gasUsed = GAS_PER_CALL;
         uint256 baseFee = block.basefee;
         uint256 royaltyWei = gasUsed * baseFee * 95 * 25 / 1_000_000; // 0.95 multiplier
 
+        // signals length must be power-of-two for FFT
+        require(hashDiffs.length > 0 && (hashDiffs.length & (hashDiffs.length - 1)) == 0, "LexNFT: length not power-of-two");
+
         uint256[] memory distances = new uint256[](hashDiffs.length);
         uint256[] memory sorted = new uint256[](hashDiffs.length);
         for (uint256 i = 0; i < hashDiffs.length; i++) sorted[i] = hashDiffs[i];
+        // median of power-of-two → middle element
         uint256 median = sorted[sorted.length / 2];
         for (uint256 i = 0; i < hashDiffs.length; i++) {
             int256 diff = int256(hashDiffs[i]) - int256(median);
