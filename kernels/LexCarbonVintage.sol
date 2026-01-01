2cce2d85cb8185dfad5e9a3dfed91f481b478824
A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>
2026-01-01 15:42:03 -0700

    Update LexCarbonVintage.sol
    Signed-off-by: A.T.W.W. <rmj95fgb7x@privaterelay.appleid.com>

--- kernels/LexCarbonVintage.sol
+++ kernels/LexCarbonVintage.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.8.25;
+import "../src/AdaptiveKernelBase.sol";
 import "../src/RoyaltySplitter.sol";
-contract LexCarbonVintage is RoyaltySplitter {
+
+contract LexCarbonVintage is RoyaltySplitter, AdaptiveKernelBase {
     uint256 public constant MAX_VINTAGE_AGE_YEARS = 5; // ≤ 5 years old
     uint256 public constant GAS_PER_CALL          = 75_000;
+
     constructor(address _beneficiary) RoyaltySplitter(_beneficiary) {}
-    function checkCarbonVintage(uint256 ageYears) external payable {
-        uint256 royaltyWei = (75_000 * block.basefee * 75 * 25) / 1_000_000;
-        if (ageYears > MAX_VINTAGE_AGE_YEARS) _splitRoyalty{value: royaltyWei}();
+
+    /// @param ageYears  Years since carbon-credit vintage
+    function checkCarbonVintage(uint256 ageYears) external payable returns (uint256 fused) {
+        uint256 gasUsed = GAS_PER_CALL;
+        uint256 baseFee = block.basefee;
+        uint256 royaltyWei = gasUsed * baseFee * 75 * 25 / 1_000_000; // 0.75 multiplier
+
+        uint256[] memory signals = new uint256[](1);
+        signals[0] = ageYears;
+
+        uint256[] memory distances = new uint256[](1);
+        distances[0] = ageYears;
+
+        uint256[] memory weights = adaptiveWeights(distances);
+        fused = signals[0] * weights[0] / 10000;
+
+        if (ageYears > MAX_VINTAGE_AGE_YEARS) {
+            _splitRoyalty{value: royaltyWei}();
+        }
     }
-    function vertical() external pure returns (string memory) { return "LexCarbonVintage-Corporate"; }
+
+    function vertical() external pure returns (string memory) {
+        return "LexCarbonVintage-Adaptive";
+    }
 }
